a1046da2985430b99d974776b0f592c6ee898e52
2025-CCE-E-DEV-038 <2025-CCE-E-DEV-038>
2026-01-31 19:53:44 +0100

    refactor: flatten nested conditional logic in the calculator

--- src/main/java/com/bnppf/developmentbooks/domain/service/BasketPriceCalculator.java
+++ src/main/java/com/bnppf/developmentbooks/domain/service/BasketPriceCalculator.java
@@ -3,25 +3,23 @@ package com.bnppf.developmentbooks.domain.service;
 import com.bnppf.developmentbooks.domain.model.ShoppingBasket;
 
 import java.math.BigDecimal;
+import java.util.Map;
 
 public class BasketPriceCalculator {
 
     private static final BigDecimal BOOK_PRICE = BigDecimal.valueOf(50);
-    private static final BigDecimal DISCOUNT_MULTIPLIER_2_BOOKS = BigDecimal.valueOf(0.95);
-    private static final BigDecimal DISCOUNT_MULTIPLIER_3_BOOKS = BigDecimal.valueOf(0.90);
+    private static final Map<Integer, BigDecimal> DISCOUNT_MULTIPLIER_BY_NUMBER_OF_BOOKS = Map.of(
+            2, BigDecimal.valueOf(0.95),
+            3, BigDecimal.valueOf(0.90),
+            4, BigDecimal.valueOf(0.80));
 
     public BigDecimal computePrice(ShoppingBasket basket) {
         int numberOfBooks = basket.getBooks().size();
-        long numberOfDifferentBooks = basket.getBooks().stream().distinct().count();
+        int numberOfDifferentBooks = (int) basket.getBooks().stream().distinct().count();
 
-        if (numberOfBooks == numberOfDifferentBooks && numberOfDifferentBooks == 2) {
-            return BOOK_PRICE.multiply(BigDecimal.valueOf(numberOfBooks)).multiply(DISCOUNT_MULTIPLIER_2_BOOKS);
-        }
-        if (numberOfBooks == numberOfDifferentBooks && numberOfDifferentBooks == 3) {
-            return BOOK_PRICE.multiply(BigDecimal.valueOf(numberOfBooks)).multiply(DISCOUNT_MULTIPLIER_3_BOOKS);
-        }
-        if (numberOfBooks == numberOfDifferentBooks && numberOfDifferentBooks == 4) {
-            return BOOK_PRICE.multiply(BigDecimal.valueOf(numberOfBooks)).multiply(BigDecimal.valueOf(0.80));
+        if (numberOfBooks == numberOfDifferentBooks && DISCOUNT_MULTIPLIER_BY_NUMBER_OF_BOOKS.containsKey(numberOfDifferentBooks)) {
+            BigDecimal discountMultiplier = DISCOUNT_MULTIPLIER_BY_NUMBER_OF_BOOKS.get(numberOfDifferentBooks);
+            return BOOK_PRICE.multiply(BigDecimal.valueOf(numberOfBooks)).multiply(discountMultiplier);
         }
         return BOOK_PRICE.multiply(BigDecimal.valueOf(numberOfBooks));
     }
