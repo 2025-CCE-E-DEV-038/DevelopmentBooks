a4d27013e7891c83a0511e46250885c76b0442bb
2025-CCE-E-DEV-038 <2025-CCE-E-DEV-038>
2026-01-31 19:47:16 +0100

    refactor: extracted constant for 3 books discount

--- src/main/java/com/bnppf/developmentbooks/domain/service/BasketPriceCalculator.java
+++ src/main/java/com/bnppf/developmentbooks/domain/service/BasketPriceCalculator.java
@@ -8,6 +8,7 @@ public class BasketPriceCalculator {
 
     private static final BigDecimal BOOK_PRICE = BigDecimal.valueOf(50);
     private static final BigDecimal DISCOUNT_MULTIPLIER_2_BOOKS = BigDecimal.valueOf(0.95);
+    private static final BigDecimal DISCOUNT_MULTIPLIER_3_BOOKS = BigDecimal.valueOf(0.90);
 
     public BigDecimal computePrice(ShoppingBasket basket) {
         int numberOfBooks = basket.getBooks().size();
@@ -17,7 +18,7 @@ public class BasketPriceCalculator {
             return BOOK_PRICE.multiply(BigDecimal.valueOf(numberOfBooks)).multiply(DISCOUNT_MULTIPLIER_2_BOOKS);
         }
         if (numberOfBooks == numberOfDifferentBooks && numberOfDifferentBooks == 3) {
-            return BOOK_PRICE.multiply(BigDecimal.valueOf(numberOfBooks)).multiply(BigDecimal.valueOf(0.90));
+            return BOOK_PRICE.multiply(BigDecimal.valueOf(numberOfBooks)).multiply(DISCOUNT_MULTIPLIER_3_BOOKS);
         }
         return BOOK_PRICE.multiply(BigDecimal.valueOf(numberOfBooks));
     }
