f9fa1c09fe45e5fab706a51fbc038419c9c9cf54
2025-CCE-E-DEV-038 <2025-CCE-E-DEV-038>
2026-02-02 13:23:11 +0100

    feat: implement logic to return a clear message when calling the endpoint with an empty book list

--- src/main/java/com/bnppf/developmentbooks/infrastructure/api/dto/ShoppingBasketRequest.java
+++ src/main/java/com/bnppf/developmentbooks/infrastructure/api/dto/ShoppingBasketRequest.java
@@ -1,9 +1,11 @@
 package com.bnppf.developmentbooks.infrastructure.api.dto;
 
 import com.bnppf.developmentbooks.infrastructure.api.validation.ValidationErrors;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
 public record ShoppingBasketRequest(@NotNull(message = ValidationErrors.SHOPPING_BASKET_NULL_LIST_ERROR)
+                                    @NotEmpty(message = ValidationErrors.SHOPPING_BASKET_EMPTY_LIST_ERROR)
                                     List<String> books) {}
